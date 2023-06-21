1320c0a76c0df5f39e9e6f42d1936e8409aad16d
Lakshya Mahawar <83651022+lakshyamahawar14@users.noreply.github.com>
2023-06-21 15:23:32 +0530

    adding cheerio instead of puppeeteer

--- api/v1/news/data/dao/newsDao.mjs
+++ api/v1/news/data/dao/newsDao.mjs
@@ -29,46 +29,81 @@ const fetchNews = async (req, res, searchQuery, numberOfResults) => {
   return response.articles;
 };
 
-import puppeteer from 'puppeteer';
+// import puppeteer from 'puppeteer';
+// 
+// const fetchArticles = async (req, res, searchQuery, numberOfResults) => {
+//   try {
+//     const browser = await puppeteer.launch({ headless: 'new' });
+//     const page = await browser.newPage();
+
+//     await page.setRequestInterception(true);
+//     page.on('request', (request) => {
+//       const resourceType = request.resourceType();
+//       if (resourceType === 'document') {
+//         request.continue();
+//       } else {
+//         request.abort();
+//       }
+//     });
+//     const url = `https://www.everydayhealth.com/search/?q=${searchQuery}&updateesi=true`;
+//     const articleTag = '.result-item';
+//     await page.goto(url, { waitUntil: 'networkidle0' });
+//     const data = await page.evaluate((articleTag, numberOfResults) => {
+//       const articleElements = Array.from(document.querySelectorAll(articleTag)).slice(0, numberOfResults);
+//       const data = [];
+//       for (const articleElement of articleElements) {
+//         const titleElement = articleElement.querySelector('.result-item__title > a');
+//         const descriptionElement = articleElement.querySelector('.result-item__description');
+//         const dateElement = articleElement.querySelector('.result-item__date');
+//         const title = titleElement ? titleElement.textContent.trim() : '';
+//         const url = titleElement ? titleElement.href : '';
+//         const description = descriptionElement ? descriptionElement.textContent.trim() : '';
+//         const date = dateElement ? dateElement.textContent.trim() : '';
+//         data.push({ title, url, description, date });
+//       }
+//       return data;
+//     }, articleTag, numberOfResults);
+//     await browser.close();
+//     return data;
+//   } catch (error) {
+//     console.error('Error scraping page:', error);
+//     throw new Error('An error occurred while scraping the page.');
+//   }
+// }
+
+import cheerio from 'cheerio'
+import fetch from 'node-fetch';
+
 
 const fetchArticles = async (req, res, searchQuery, numberOfResults) => {
   try {
-    const browser = await puppeteer.launch({ headless: 'new' });
-    const page = await browser.newPage();
-
-    await page.setRequestInterception(true);
-    page.on('request', (request) => {
-      const resourceType = request.resourceType();
-      if (resourceType === 'document') {
-        request.continue();
-      } else {
-        request.abort();
-      }
-    });
     const url = `https://www.everydayhealth.com/search/?q=${searchQuery}&updateesi=true`;
+    const response = await fetch(url);
+    const html = await response.text();
+    const $ = cheerio.load(html);
+
     const articleTag = '.result-item';
-    await page.goto(url, { waitUntil: 'networkidle0' });
-    const data = await page.evaluate((articleTag, numberOfResults) => {
-      const articleElements = Array.from(document.querySelectorAll(articleTag)).slice(0, numberOfResults);
-      const data = [];
-      for (const articleElement of articleElements) {
-        const titleElement = articleElement.querySelector('.result-item__title > a');
-        const descriptionElement = articleElement.querySelector('.result-item__description');
-        const dateElement = articleElement.querySelector('.result-item__date');
-        const title = titleElement ? titleElement.textContent.trim() : '';
-        const url = titleElement ? titleElement.href : '';
-        const description = descriptionElement ? descriptionElement.textContent.trim() : '';
-        const date = dateElement ? dateElement.textContent.trim() : '';
-        data.push({ title, url, description, date });
-      }
-      return data;
-    }, articleTag, numberOfResults);
-    await browser.close();
+    const articleElements = $(articleTag).slice(0, numberOfResults);
+    const data = [];
+
+    articleElements.each((index, element) => {
+      const titleElement = $(element).find('.result-item__title > a');
+      const descriptionElement = $(element).find('.result-item__description');
+      const dateElement = $(element).find('.result-item__date');
+
+      const title = titleElement.text().trim();
+      const url = titleElement.attr('href');
+      const description = descriptionElement.text().trim();
+      const date = dateElement.text().trim();
+
+      data.push({ title, url, description, date });
+    });
+
     return data;
   } catch (error) {
     console.error('Error scraping page:', error);
     throw new Error('An error occurred while scraping the page.');
   }
-}
+};
 
 export { fetchNews, fetchArticles };
