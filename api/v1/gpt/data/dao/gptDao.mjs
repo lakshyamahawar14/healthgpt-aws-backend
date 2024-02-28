3b6d7d75b8eea6877831fadbbaec98e9e280a3b5
rohitjingar <rohitjingar264@gmail.com>
2024-02-28 14:35:17 +0530

    some algo changes

--- api/v1/gpt/data/dao/gptDao.mjs
+++ api/v1/gpt/data/dao/gptDao.mjs
@@ -18,7 +18,9 @@ const envPath = resolve(
 dotenv.config({ path: envPath });
 
 import axios from "axios";
-
+const chooseRandomPrompt = (prompts) => {
+  return prompts[Math.floor(Math.random() * prompts.length)];
+};
 const fetchUserChat = async (req, res, userId, accessToken) => {
   const response = await axios.get(
     `http://localhost:${process.env.DATABASE_API_PORT}/api/v1/db/chat?userId=${userId}&accessToken=${accessToken}`
@@ -66,7 +68,7 @@ const getSystemRoleMessage = async (
   let positiveBeliefString =
     ` You should engage the user in cognitive restructuring techniques to challenge and convert those irrational beliefs into rational ones which provide a positive emotion to the user. This can involve providing counter-evidence, reframing the situation, offering alternative perspectives, or providing affirmations and positive statements.`;
   let askInDeepString =
-    ` You should ask the user in deep about his beliefs like what the user is experiencing and how it's affecting him/her.`;
+    ` You should ask the user in deep about his beliefs like what the user is experiencing.`;
   let useBrahmastraString =
     ` The user is not telling anything. Meaning, maybe the user is not having any symptoms or not able to identify what user belief is. Ask the user if he is able to do so. Also tell the user that you won't be able to help him/her further if he/she doesn't tell about his/her beliefs.`;
 
@@ -77,27 +79,39 @@ const getSystemRoleMessage = async (
   let isNegativeBelief = belief.includes("No") || belief.includes("no") ? false : true;
   let systemRoleMessage = defaultString;
   let numberOfMessages = numMsg || chat.length;
-  if (numberOfMessages < 3) {
-    systemRoleMessage = defaultString;
-  } else if (numberOfMessages < 5) {
-    if (isNegativeBelief === true) {
-      systemRoleMessage += ` User is identiefied to be having irrational beliefs as '${belief}'`+ askInDeepString;
-    } else {
-      systemRoleMessage += oldUserInstruction + ` Try to gather information more information about what symptoms or beliefs user might be expericencing.`;
-    }
-  } else if (numberOfMessages < 10) {
-    if (isNegativeBelief === true) {
-      systemRoleMessage += positiveBeliefString+` User is identiefied to be having irrational beliefs as '${belief}'. If these beliefs are sufficient to classify the disorder, Try to assist the user by telling the solution and converting his beliefs into rational ones. Otherwise, go try to gather more information about user's symptoms and beliefs.`;
-    } else {
-      systemRoleMessage += newUserInstruction;
-    }
-  } else {
-    if (isNegativeBelief === true) {
-      systemRoleMessage += positiveBeliefString+` User is identiefied to be having irrational beliefs as '${belief}'. Try to assist the user by telling the solution and converting his beliefs into rational ones. You can give him tips to recover those symptoms or beliefs.`;
-    } else {
-      systemRoleMessage += useBrahmastraString;
-    }
+  console.log(numberOfMessages)
+
+  if(isNegativeBelief===true){
+    console.log(belief)
+    systemRoleMessage += ` User is identiefied to be having irrational beliefs as '${belief}'`+ askInDeepString;
+
   }
+  else{
+     systemRoleMessage = defaultString;
+  }
+
+  // if (numberOfMessages < 3) {
+  //   systemRoleMessage = defaultString;
+  // } else if (numberOfMessages < 5) {
+  //   if (isNegativeBelief === true) {
+  //     console.log(belief)
+  //     systemRoleMessage += ` User is identiefied to be having irrational beliefs as '${belief}'`+ askInDeepString;
+  //   } else {
+  //     systemRoleMessage += oldUserInstruction + ` Try to gather information more information about what symptoms or beliefs user might be expericencing.`;
+  //   }
+  // } else if (numberOfMessages < 10) {
+  //   if (isNegativeBelief === true) {
+  //     systemRoleMessage += positiveBeliefString+` User is identiefied to be having irrational beliefs as '${belief}'. If these beliefs are sufficient to classify the disorder, Try to assist the user by telling the solution and converting his beliefs into rational ones. Otherwise, go try to gather more information about user's symptoms and beliefs.`;
+  //   } else {
+  //     systemRoleMessage += newUserInstruction;
+  //   }
+  // } else {
+  //   if (isNegativeBelief === true) {
+  //     systemRoleMessage += positiveBeliefString+` User is identiefied to be having irrational beliefs as '${belief}'. Try to assist the user by telling the solution and converting his beliefs into rational ones. You can give him tips to recover those symptoms or beliefs.`;
+  //   } else {
+  //     systemRoleMessage += useBrahmastraString;
+  //   }
+  // }
   return systemRoleMessage;
 };
 
